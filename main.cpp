451c4659470d1d3ccfcb9f873d148385174f7f80
Babacar Fall <Fallbaba136@gmail.com>
2025-09-23 15:43:29 +0200

    Nouvelles fonctions ajoutee

--- main.cpp
+++ main.cpp
@@ -1,14 +1,102 @@
-//
-//  main.cpp
-//  TP3Info4
-//
-//  Created by Fall on 23/09/2025.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//CONSTRUCTION D'UN ARBRE
+
+struct noeud
+{
+    string nom;
+    int nbr_fils;
+    noeud** tab;
+};
+void createArbre(noeud** racine)
+{
+    noeud* newNode = new noeud;
+    cout << "Entrer le nom de la personne : ";
+    cin >> newNode->nom;
+    cout << "Saisissez le nombre d'enfant : ";
+    cin >> newNode->nbr_fils;
+    *racine = newNode;
+    newNode->tab = new noeud*[newNode->nbr_fils];
+    for (int i = 0; i < newNode->nbr_fils; i++)
+    {
+        newNode->tab[i] = NULL;
+    }
+    if (newNode->nbr_fils == 0) return;
+    else{
+        for (int i = 0; i < newNode->nbr_fils; i++)
+        {
+            createArbre(&(*racine)->tab[i]);
+        }
+    }
+    
+}
+
+//AFFICHAGE DES ARBRES
+void afficheNoeud(noeud* racine, int prof)
+{
+    if (racine == NULL) return;
+    else
+    {
+        cout << racine->nom;
+        if (prof != 0)
+        {
+            cout << "\t";
+        }
+        for (int i = 0; i < racine->nbr_fils; i++)
+        {
+            afficheNoeud(racine->tab[i], prof + 1);
+        }
+       
+    }
+}
+
+//recher
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, const char * argv[])
+{
+    noeud* newNoeud = NULL;
+    createArbre(&newNoeud);
+    afficheNoeud(newNoeud, 4);
+    
+    
+    
     return 0;
 }
